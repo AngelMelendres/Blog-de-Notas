3f6df3b34e8b251760d511a8b790ac3c96ec6bc2
AngelMelendres <fabian_melendres@hotmail.com>
2021-06-30 15:52:27 -0500

    comc

--- app.js
+++ app.js
@@ -24,10 +24,39 @@ function agregarNota(e){
     
     lista.appendChild(li);
     li.appendChild(borrar);
+
+    agregarLocalStorange(nota);
     
 }
 
+//AGREGAR A LOCAL STORANGE
 
+function agregarLocalStorange(nota){
+    let notas;
+    notas=agregarLs();
+
+    notas.push(nota);
+    localStorage.setItem("notas", JSON.stringify(notas));
+    localStorage.setItem("notas", nota);
+
+}
+
+function agregarLs(){
+    let notas;
+
+        if(localStorage.getItem("notas")===null){
+            notas=[];
+        }
+        else{
+            notas=JSON.parse(localStorage.getItem("notass"));
+        }
+        return notas;
+
+}
+
+
+
+//ELIMINAR NOTA
 function eliminarNota(e){
     e.preventDefault();
     if(e.target.className==="borrar"){
